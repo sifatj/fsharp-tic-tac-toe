c27c67e5b5f86cde979deb867e4f600db1aa3cfc
Dragos-Andrei Ilies <Dragos-Andrei.Ilies@mail.bcu.ac.uk>
2019-05-13 16:02:14 +0100

    small test

--- TicTacToe_Game/TicTacToe_Game/GameLocal.fs
+++ TicTacToe_Game/TicTacToe_Game/GameLocal.fs
@@ -10,6 +10,7 @@ module GameLocal =
             let xpos = Console.ReadLine() |> int
             printfn "Please input the column number"
             let ypos = Console.ReadLine() |> int
+            //Check if position can  be put
             let newGrid = getNewGrid grid xpos ypos token
             let status = checkGridStatus (P token) newGrid
             match status with
