f3f509801945771c5530c774e08b6b23a56cd463
DragosIlies <ilies9dragos@gmail.com>
2019-05-17 09:12:25 +0100

    added SS tests

--- TicTacToe_Game/TocTacToeGame_Tests/Library1.fs
+++ TicTacToe_Game/TocTacToeGame_Tests/Library1.fs
@@ -3,6 +3,9 @@ module TicTacToeGame =
     open NUnit.Framework
     open TicTacToe_Game.Helper
     open TicTacToe_Game.Command
+    open TicTacToe_Game.EasyAIGame
+    open TicTacToe_Game.HardAIGame
+    open FsCheck
 
     [<Test>]
     let ``Check parsing a string to integer`` () =
@@ -98,4 +101,49 @@ module TicTacToeGame =
         let actual = calculateWinLossRatio playerRecord
         Assert.AreNotEqual(expected, actual)
 
+
+    let grid = 
+        array2D [
+            [ Empty; P X; Empty ]
+            [ Empty; Empty; Empty ]
+            [ Empty; Empty; Empty ]
+        ]
+
+    let updatedGrid = 
+        array2D [
+            [ P O; P X; Empty ]
+            [ Empty; Empty; Empty ]
+            [ Empty; Empty; Empty ]
+        ]
+
+    [<Test>]
+    let ``add token using updateGrid`` () = 
+        Assert.AreEqual(updatedGrid, updateGrid grid (0,0) O)
+
+    [<Test>]
+    let ``find all Empty cells in grid`` () =
+        let result = seq [(Empty, 0); (Empty, 2); (Empty, 3); (Empty, 4); (Empty, 5); (Empty, 6); (Empty, 7); (Empty, 8);]
+        Assert.AreEqual (result, isEmpty grid)
+
+    [<Test>]
+    let ``pick random value from sequence of available cells`` () = 
+        let availableCells = seq [(0, 0); (0, 2); (1, 0); (1, 1); (1, 2); (2, 0); (2, 1); (2, 2);]
+        let prop_tupleRange (x, y) =
+            (x>0 && x<=2 && y>0 && y<=2) 
+        let prop_ContainedInSequence xs =
+            easyAI xs |> fun cell -> Seq.contains cell xs
+
+        Assert.IsTrue (prop_ContainedInSequence availableCells)
+
+
+    let blockGrid = 
+        array2D [
+            [ P O; P X; Empty ]
+            [ Empty; P X; Empty ]
+            [ Empty; Empty; Empty ]
+        ]
+
+    [<Test>]
+    let ``pick right move to block player from win`` () =
+        Assert.AreEqual((true, (2,1)), block (P X) blockGrid)
    
