d59f2cfa30155f6a559e659b2ab4493fad41c4ec
Sifat Jahir <sifatjahir@hotmail.com>
2019-05-17 08:54:21 +0100

    added some leaderboard tests

--- TicTacToe_Game/TocTacToeGame_Tests/Library1.fs
+++ TicTacToe_Game/TocTacToeGame_Tests/Library1.fs
@@ -59,3 +59,41 @@ module TicTacToeGame =
                 [ P X; P X; Empty ]
             ]
         Assert.AreEqual(newGrid,getNewGrid grid (0,0) X )
+
+
+
+
+    (* Leaderboard.fs file tests *)
+
+    // Dummy Record 
+    type TestPlayerRecord = {
+        PlayerName: string
+        Wins: int
+        Losses: int 
+    }
+
+    [<Test>]
+    let ``Calculate win loss ratio correctly`` () =
+        let playerRecord:TestPlayerRecord = {
+            PlayerName = "sifat"
+            Wins = 1
+            Losses = 1 
+        }
+        let calculateWinLossRatio player = 
+            (float player.Wins / float (player.Wins + player.Losses))
+        let expected = float (1) / float (1+1)
+        let actual = calculateWinLossRatio playerRecord
+        Assert.AreEqual(expected, actual)
+
+    [<Test>]
+    let ``Calculate win loss ratio incorrectly`` () =
+        let playerRecord:TestPlayerRecord = {
+            PlayerName = "sifat"
+            Wins = 1
+            Losses = 2 
+        }
+        let calculateWinLossRatio player = 
+            (float player.Wins / float (player.Wins + player.Losses))
+        let expected = float (1) / float (1 + 4)
+        let actual = calculateWinLossRatio playerRecord
+        Assert.AreNotEqual(expected, actual)
